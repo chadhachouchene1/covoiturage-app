d98c072bac69ba4c8e3dec98e512dd0cef2ca66f
chouchenechadha1 <chouchenechadha01@gmail.com>
2026-06-24 17:29:53 +0100

    modifie le test

--- backend/_tests_/index.test.js
+++ backend/_tests_/index.test.js
@@ -1,10 +1,22 @@
+// Mock AWS avant tout require !
+jest.mock('@aws-sdk/client-s3', () => ({
+  S3Client: jest.fn().mockImplementation(() => ({})),
+  PutObjectCommand: jest.fn(),
+  DeleteObjectCommand: jest.fn(),
+}));
+
+jest.mock('multer-s3', () => () => ({
+  _handleFile: jest.fn(),
+  _removeFile: jest.fn(),
+}));
+
 const request = require('supertest');
-const { app } = require('../index'); // Assure-toi que ton fichier server.js exporte bien 'app'
+const { app } = require('../index');
 
 describe("Express App Tests", () => {
   it("should return 200 OK on GET /", async () => {
     const res = await request(app).get("/");
-    expect(res.status).toBe(200); // Corrigé ici ;
+    expect(res.status).toBe(200);
     expect(res.text).toBe("serveur is running");
   });
 });
